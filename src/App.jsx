b0139d081af25fb1430ebe2bb591007af0f84e95
Dallin Covey <covey.dallin@gmail.com>
2025-11-18 11:33:51 -0700

    feat: signup page with link to/from home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Blog } from './Blog.jsx'
+import { Blog } from './pages/Blog.jsx'
+import { Signup } from './pages/Signup.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 const queryClient = new QueryClient()
 const router = createBrowserRouter([
@@ -7,7 +8,12 @@ const router = createBrowserRouter([
     path: '/',
     element: <Blog />,
   },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
 ])
+
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
