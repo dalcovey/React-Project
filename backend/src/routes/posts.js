c7a228a7186a56f62af86439b9d14d12b973e785
Dallin Covey <covey.dallin@gmail.com>
2025-11-18 10:49:42 -0700

    chore: make author associated with id

--- backend/src/routes/posts.js
+++ backend/src/routes/posts.js
@@ -42,7 +42,7 @@ export function postsRoutes(app) {
   })
   app.post('/api/v1/posts', requireAuth, async (req, res) => {
     try {
-      const post = await createPost(req.body)
+      const post = await createPost(req.auth.sub, req.body)
       return res.json(post)
     } catch (err) {
       console.error('error creating post', err)
@@ -51,7 +51,7 @@ export function postsRoutes(app) {
   })
   app.patch('/api/v1/posts/:id', requireAuth, async (req, res) => {
     try {
-      const post = await updatePost(req.params.id, req.body)
+      const post = await updatePost(req.auth.sub, req.params.id, req.body)
       return res.json(post)
     } catch (err) {
       console.error('error updating post', err)
@@ -60,7 +60,7 @@ export function postsRoutes(app) {
   })
   app.delete('/api/v1/posts/:id', requireAuth, async (req, res) => {
     try {
-      const { deletedCount } = await deletePost(req.params.id)
+      const { deletedCount } = await deletePost(req.auth.sub, req.params.id)
       if (deletedCount === 0) return res.sendStatus(404)
       return res.status(204).end()
     } catch (err) {
